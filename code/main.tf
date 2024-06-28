628b9ee1671d849eaab32b068ada13ab3b9d7426
tomasbogalho <tomasbogalho98@gmail.com>
2024-06-28 19:54:02 +0100

    had a count mistake, fixed

--- code/main.tf
+++ code/main.tf
@@ -115,38 +115,9 @@ resource "azurerm_windows_virtual_machine" "avd_vm" {
   ]
 }
 
-resource "azurerm_virtual_machine_extension" "domain_join" {
-  count                      = var.rdsh_count
-  name                       = "${var.prefix}-${count.index + 1}-domainJoin"
-  virtual_machine_id         = azurerm_windows_virtual_machine.avd_vm.*.id[count.index]
-  publisher                  = "Microsoft.Compute"
-  type                       = "JsonADDomainExtension"
-  type_handler_version       = "1.3"
-  auto_upgrade_minor_version = true
-
-  settings = <<SETTINGS
-    {
-      "Name": "${var.domain_name}",
-      "OUPath": "${var.ou_path}",
-      "User": "${var.domain_user_upn}@${var.domain_name}",
-      "Restart": "true",
-      "Options": "3"
-    }
-SETTINGS
-
-  protected_settings = <<PROTECTED_SETTINGS
-    {
-      "Password": "${var.domain_password}"
-    }
-PROTECTED_SETTINGS
-
-  lifecycle {
-    ignore_changes = [settings, protected_settings]
-  }
-
-}
 
 resource "azurerm_virtual_machine_extension" "aad_login" {
+  count                      = var.rdsh_count
   name                 = "AADLogin"
   virtual_machine_id   = azurerm_windows_virtual_machine.avd_vm.*.id[count.index]
   publisher            = "Microsoft.Azure.ActiveDirectory"
