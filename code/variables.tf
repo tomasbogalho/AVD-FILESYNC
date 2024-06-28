e48548982bdb417a98393a0cd3494fc9b4a16784
tomasbogalho <tomasbogalho98@gmail.com>
2024-06-28 11:14:35 +0100

    new terraform format applied

--- code/variables.tf
+++ code/variables.tf
@@ -1,34 +1,34 @@
 variable "resource_group_location" {
-default     = "West Europe"
-description = "Location of the resource group."
+  default     = "West Europe"
+  description = "Location of the resource group."
 }
 
 variable "rg_name" {
-type        = string
-default     = "rg-avd-resources"
-description = "Name of the Resource group in which to deploy service objects"
+  type        = string
+  default     = "rg-avd-resources"
+  description = "Name of the Resource group in which to deploy service objects"
 }
 
 variable "workspace" {
-type        = string
-description = "Name of the Azure Virtual Desktop workspace"
-default     = "AVD TF Workspace"
+  type        = string
+  description = "Name of the Azure Virtual Desktop workspace"
+  default     = "AVD TF Workspace"
 }
 
 variable "hostpool" {
-type        = string
-description = "Name of the Azure Virtual Desktop host pool"
-default     = "AVD-TF-HP"
+  type        = string
+  description = "Name of the Azure Virtual Desktop host pool"
+  default     = "AVD-TF-HP"
 }
 
 variable "rfc3339" {
-type        = string
-default     = "2022-03-30T12:43:13Z"
-description = "Registration token expiration"
+  type        = string
+  default     = "2022-03-30T12:43:13Z"
+  description = "Registration token expiration"
 }
 
 variable "prefix" {
-type        = string
-default     = "avdtf"
-description = "Prefix of the name of the AVD machine(s)"
+  type        = string
+  default     = "avdtf"
+  description = "Prefix of the name of the AVD machine(s)"
 }
