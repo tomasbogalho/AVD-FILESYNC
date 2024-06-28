51b62c371870dcc985ebd94169b775d8d57eacdd
tomasbogalho <tomasbogalho98@gmail.com>
2024-06-28 12:10:31 +0100

    adding main file

--- code/main.tf
+++ code/main.tf
@@ -29,29 +29,6 @@ resource "azurerm_virtual_desktop_host_pool" "hostpool" {
 
 resource "azurerm_virtual_desktop_host_pool_registration_info" "registrationinfo" {
   hostpool_id     = azurerm_virtual_desktop_host_pool.hostpool.id
-  expiration_date = timestampadd("h", 1, timestamp()) # Set expiration time to 1 hour from now
-}
-
-# Create AVD DAG
-resource "azurerm_virtual_desktop_application_group" "dag" {
-  resource_group_name = azurerm_resource_group.sh.name
-  host_pool_id        = azurerm_virtual_desktop_host_pool.hostpool.id
-  location            = azurerm_resource_group.sh.location
-  type                = "Desktop"
-  name                = "${var.prefix}-dag"
-  friendly_name       = "Desktop AppGroup"
-  description         = "AVD application group"
-  depends_on          = [azurerm_virtual_desktop_host_pool.hostpool, azurerm_virtual_desktop_workspace.workspace]
-}
-
-# Associate Workspace and DAG
-resource "azurerm_virtual_desktop_workspace_application_group_association" "ws-dag" {
-  application_group_id = azurerm_virtual_desktop_application_group.dag.id
-  workspace_id         = azurerm_virtual_desktop_workspace.workspace.id
-}
-
-resource "azurerm_virtual_desktop_host_pool_registration_info" "registrationinfo" {
-  hostpool_id     = azurerm_virtual_desktop_host_pool.hostpool.id
   expiration_date = var.rfc3339
 }
 
