7968d98e7f68a6e4ed0d31ec104145db62ec267c
tomasbogalho <tomasbogalho98@gmail.com>
2024-06-29 18:22:24 +0100

    added dependencies for resources, peering between storage vnet and onprem vnet, renamed the peering

--- code/main.tf
+++ code/main.tf
@@ -236,7 +236,7 @@ resource "azurerm_subnet" "name" {
 
 
 #creating a peering between avd vent and storage account vnet 
-resource "azurerm_virtual_network_peering" "peering" {
+resource "azurerm_virtual_network_peering" "peering1" {
   name                         = "avd-vnet-to-storage-account-vnet"
   resource_group_name          = azurerm_resource_group.sh.name
   virtual_network_name         = azurerm_virtual_network.vnet.name
@@ -251,7 +251,7 @@ resource "azurerm_virtual_network_peering" "peering" {
 }
 
 #creating the peering between storage account vnet and avd vnet
-resource "azurerm_virtual_network_peering" "name" {
+resource "azurerm_virtual_network_peering" "peering2" {
   name                         = "storage-account-vnet-to-avd-vnet"
   resource_group_name          = azurerm_resource_group.rg_sa.name
   virtual_network_name         = azurerm_virtual_network.storage_account_vnet.name
@@ -267,7 +267,7 @@ resource "azurerm_virtual_network_peering" "name" {
 }
 
 #creating the peering between storage account vnet and onprem vnet
-resource "azurerm_virtual_network_peering" "name" {
+resource "azurerm_virtual_network_peering" "peering3" {
   name                         = "storage-account-vnet-to-onprem-vnet"
   resource_group_name          = azurerm_resource_group.rg_sa.name
   virtual_network_name         = azurerm_virtual_network.storage_account_vnet.name
@@ -282,7 +282,7 @@ resource "azurerm_virtual_network_peering" "name" {
 
 }
 # creating the peering between onprem vnet and storage account vnet
-resource "azurerm_virtual_network_peering" "name" {
+resource "azurerm_virtual_network_peering" "peering4" {
   name                         = "onprem-vnet-to-storage-account-vnet"
   resource_group_name          = azurerm_resource_group.rg_onprem.name
   virtual_network_name         = azurerm_virtual_network.onprem_vnet.name
