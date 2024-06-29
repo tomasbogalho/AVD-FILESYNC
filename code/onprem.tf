e800cf34c2fa8712ac3a9c92c88867d10a3aa15f
tomasbogalho <tomasbogalho98@gmail.com>
2024-06-29 23:48:18 +0100

    puting bastion in the right vnet

--- code/onprem.tf
+++ code/onprem.tf
@@ -51,7 +51,7 @@ resource "azurerm_bastion_host" "bastion" {
 
   ip_configuration {
     name                 = "configuration"
-    subnet_id            = azurerm_subnet.onprem_subnet.id
+    subnet_id            = azurerm_subnet.bastion_subnet.id
     public_ip_address_id = azurerm_public_ip.bastion_public_ip.id
   }
 }
