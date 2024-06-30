3ac6169937661d5507912d236a1e27346b3ff785
tomasbogalho <tomasbogalho98@gmail.com>
2024-06-30 14:34:03 +0100

    not using module, testing by hand

--- code/onprem.tf
+++ code/onprem.tf
@@ -140,7 +140,6 @@ resource "azurerm_windows_virtual_machine" "file_sync_vm" {
   }
 }
 
-/*
 resource "azurerm_storage_sync" "storage_sync" {
   name                = "StorageSync"
   resource_group_name = azurerm_resource_group.rg_onprem.name
@@ -157,45 +156,5 @@ resource "azurerm_storage_sync_cloud_endpoint" "storage_sync_cloud_endpoint" {
   storage_sync_group_id = azurerm_storage_sync_group.storage_sync_group.id
   file_share_name       = azurerm_storage_share.fileshare.name
   storage_account_id    = azurerm_storage_account.sa.id
-}
-
-# Azurerm Provider configuration
-provider "azurerm" {
-  features {}
-}
-*/
-module "azure-filesync" {
-  source = "ravensorb/azure-filesync/azurerm"
-
-  # The name to use for this instance
-  name = "filesync"
-
-  # A prefix to use for all resouyrces created (if left blank, the resource group name will be used)
-  resource_prefix = "shared-westeurope"
-
-  # By default, this module will create a resource group, proivde the name here
-  # to use an existing resource group, specify the existing resource group name, 
-  # and set the argument to `create_resource_group = false`. Location will be same as existing RG. 
-  resource_group_name = "shared-westeurope-rg-filesync"
-
-  # Location to deploy into
-  location = azurerm_resource_group.rg_onprem.location
-
-  # Set to true to limit access to specific subnets.  
-  # Note: requires settings virtual_network_name, virtual_network_resource_group_name, and subnet_net
-  storage_account_limit_access_to_subnets = false
-  # VNet and Subnet details
-  # The vnet to use to deploy this into
-  #virtual_network_name                = ""
-  # The resource group name for vnet to use to deploy this into
-  #virtual_network_resource_group_name = "" # Set to null to use the sameresource group 
-  # The number of the subnet to use only needed if limited access to specific subnets
-  #subnet_name                         = ""
-
-  # Storage Account Settings
-  storage_account_tier             = "Standard"
-  storage_account_replication_type = "LRS"
-  storage_share_name               = "filesync"
-  storage_share_quota              = "100GB"
 }
 
