9947327a2f53c980a0dd96646edf6f2c1a6b6af1
tomasbogalho <tomasbogalho98@gmail.com>
2024-07-02 12:28:50 +0100

    change passwords and adapted filesync to be able to see powershell output

--- code/onprem.tf
+++ code/onprem.tf
@@ -105,15 +105,11 @@ resource "azurerm_virtual_machine_extension" "filesync_extension" {
   settings = <<SETTINGS
     {
       "fileUris": ["https://raw.githubusercontent.com/tomasbogalho/AVD-FILESYNC/main/code/RegisterFileSyncServer.ps1"],
-      "commandToExecute": "powershell -ExecutionPolicy Unrestricted -File RegisterFileSyncServer.ps1 -rgName ${var.rg_onprem} -sssName ${var.storage_sync_service_name} -fssName ${var.filesync_vm_name} -SyncGroup ${var.storage_sync_group_name}"
+      "commandToExecute": "powershell -ExecutionPolicy Unrestricted -File RegisterFileSyncServer.ps1 Out-File -filepath postBuild.ps1 -rgName ${var.rg_onprem} -sssName ${var.storage_sync_service_name} -fssName ${var.filesync_vm_name} -SyncGroup ${var.storage_sync_group_name}"
     }
   SETTINGS
 
   depends_on = [azurerm_windows_virtual_machine.file_sync_vm, azurerm_managed_disk.datadisk]
-}
-
-output "extension_command_to_execute" {
-  value = azurerm_virtual_machine_extension.filesync_extension.settings["commandToExecute"]
 }
 
 
